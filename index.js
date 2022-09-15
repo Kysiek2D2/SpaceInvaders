7c64177624a297750fb897f34a6720cabfb0db1e
Krzysztof Dąbkowski <krda@netcompany.com>
2022-09-15 11:42:12 +0200

    Script and HTML structure updated.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("test");
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 600;
+
+const background = new Image();
+background.src = "resources/images/space.png";
+
+function game() {
+    console.log("game runs...");
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+
+}
+
+setInterval(game, 1000 / 60);
